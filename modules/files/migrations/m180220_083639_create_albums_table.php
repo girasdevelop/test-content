95419bc3609dd5fc4bec30cb80daf2bdd1b5a4f8
Andrey Girnik <girnikandrey@gmail.com>
2018-02-26 16:02:49 +0600

    Remake migrations

--- modules/files/migrations/m180220_083639_create_albums_table.php
+++ modules/files/migrations/m180220_083639_create_albums_table.php
@@ -16,6 +16,9 @@ class m180220_083639_create_albums_table extends Migration
             'id' => $this->primaryKey(),
             'title' => $this->string()->notNull(),
             'description' => $this->text(),
+            'type' => $this->string(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer(),
         ]);
     }
 
